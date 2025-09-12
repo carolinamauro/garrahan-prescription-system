e73542c975ff3c6d55c70146a5e09a50fd7639f3
Federico Genaro <fedegen14@gmail.com>
2025-09-12 00:16:58 -0300

    Agrego info protocolo en detalles del paciente

--- src/app/patients/[id]/page.js
+++ src/app/patients/[id]/page.js
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import { useHeader } from '@/app/contexts/HeaderContext';
 import styles from './page.module.css';
 import InfoPatient from "@/components/InfoPatient";
+import Button from "@/components/Button";
 
 export default function PatientDetailPage({ params }) {
     const { setTitle, setSubtitle } = useHeader();
@@ -19,6 +20,19 @@ export default function PatientDetailPage({ params }) {
             </div>
 
             <InfoPatient id={params.id} />
+
+            <div className={styles.protocolInfo}>
+                <div className={styles.protocolCard}>
+                    <div className={styles.protocolGrid}>
+                        <span className={styles.label}>Protocolo de tratamiento</span>
+                        <span className={styles.value}>No tiene protocolo asignado</span>
+                    </div>
+                    <div className={styles.buttonsGrid}>
+                        <Button>Seleccionar protocolo</Button>
+                        <Button>Generar receta</Button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
