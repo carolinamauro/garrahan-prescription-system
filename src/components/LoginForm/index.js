b74969ed81022853387de16d638e033553a7ab59
Federico Genaro <fedegen14@gmail.com>
2025-11-01 16:06:12 -0300

    Saco lógica de login a LoginContext

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,64 +1,66 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
+import { useLogin } from '@/contexts/LoginContext';
+import { useState } from 'react';
 import { Button } from '@/components/ui/button';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '@/components/ui/card';
 import { Shield, User } from 'lucide-react';
+import { AlertPopup } from '@/components/AlertPopup';
 
 export function LoginForm() {
+  const [showDialog, setShowDialog] = useState(false);
   const router = useRouter();
-
-  // TODO: Mover a LoginContext
-  const login = async (role) => {
-    await fetch('http://localhost:3000/auth/login-test', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      credentials: 'include',
-      body: JSON.stringify({
-        id: role === 'admin' ? '1' : '2',
-        name: role === 'admin' ? 'Admin' : 'Dr. Juan',
-        role
-      })
-    });
-  };
+  const { loginAs } = useLogin();
 
   const handleLogin = async (role) => {
-    await login(role);
-    router.push('/');
+    try {
+      await loginAs(role);
+      router.push('/');
+    } catch (err) {
+      console.error(err);
+      setShowDialog(true);
+    }
   };
 
   return (
-    <Card className='flex flex-col gap-6 min-w-xs'>
-      <CardHeader className="text-center">
-        <CardTitle className="text-xl">Bienvenido</CardTitle>
-        <CardDescription>
-                          Selecciona tu rol para iniciar sesión
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="flex flex-col gap-4">
-        <Button
-          variant="default"
-          className="flex items-center justify-center gap-2"
-          onClick={() => handleLogin('admin')}
-        >
-          <Shield className="w-4 h-4" />
-                          Entrar como Admin
-        </Button>
-        <Button
-          variant="secondary"
-          className="flex items-center justify-center gap-2"
-          onClick={() => handleLogin('medico')}
-        >
-          <User className="w-4 h-4" />
-                          Entrar como Médico
-        </Button>
-      </CardContent>
-    </Card>
+    <>
+      <Card className='flex flex-col gap-6 min-w-xs'>
+        <CardHeader className="text-center">
+          <CardTitle className="text-xl">Bienvenido</CardTitle>
+          <CardDescription>
+                    Selecciona tu rol para iniciar sesión
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="flex flex-col gap-4">
+          <Button
+            variant="default"
+            className="flex items-center justify-center gap-2"
+            onClick={() => handleLogin('admin')}
+          >
+            <Shield className="w-4 h-4" />
+                    Entrar como Admin
+          </Button>
+          <Button
+            variant="secondary"
+            className="flex items-center justify-center gap-2"
+            onClick={() => handleLogin('medico')}
+          >
+            <User className="w-4 h-4" />
+                    Entrar como Médico
+          </Button>
+        </CardContent>
+      </Card>
+
+      <AlertPopup
+        title="Error al iniciar sesión"
+        description="Por favor intente nuevamente."
+        handleOnClick={() => {
+          router.push('/login-test');
+        }}
+        showDialog={showDialog}
+        setShowDialog={setShowDialog}
+      />
+    </>
   );
 }
--- src/contexts/LoginContext.js
+++ src/contexts/LoginContext.js
@@ -1,47 +1,44 @@
-/* global fetch */
 'use client';
-import { useContext, useEffect, useState, createContext } from 'react';
+
+import { createContext, useContext, useState } from 'react';
 
 const LoginContext = createContext();
 
-export function LoginProvider({ children }) {
+const idRolAdmin = '1';
+const idRolMedico = '2';
+const admin = 'admin';
 
-  const [login, setLogin] = useState(null);
+export function LoginProvider({ children }) {
+  const [user, setUser] = useState(null);
   const [loggedIn, setLoggedIn] = useState(false);
 
-  useEffect(() => {
-    if (!loggedIn) {
-      const storedLogin = window.localStorage.getItem('loginData');
-      if (storedLogin) {
-        setLogin(JSON.parse(storedLogin));
-      } else {
-        const login = async (role) => {
-          await fetch('http://localhost:3000/auth/login-test', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            credentials: 'include',
-            body: JSON.stringify({
-              id: role === 'admin' ? '1' : '2',
-              name: role === 'admin' ? 'Admin' : 'Dr. Juan',
-              role
-            })
-          });
-        };
-        login('medico').then(() => {
-          setLogin({ id: '2', name: 'Dr. Juan', role: 'medico' });
-          window.localStorage.
-            setItem('loginData', JSON.stringify({ id: '2', name: 'Dr. Juan', role: 'medico' }));
-        });
-      }
-      setLoggedIn(true);
+  const loginAs = async (role) => {
+    const currUser = {
+      id: role === admin ? idRolAdmin : idRolMedico,
+      name: role === admin ? 'Administrador' : 'Dr. Miguel Merentiel',
+      role
+    };
+
+    const response = await fetch('http://localhost:3000/auth/login-test', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'include',
+      body: JSON.stringify(currUser)
+    });
+
+    if (!response.ok) {
+      throw new Error('Error al iniciar sesión');
     }
-  }, [loggedIn]);
+
+    setUser(currUser);
+    setLoggedIn(true);
+  };
 
   return (
-    <LoginContext.Provider value={{login, setLogin}}>
+    <LoginContext.Provider value={{ user, loggedIn, loginAs }}>
       {children}
     </LoginContext.Provider>
   );
 }
 
-export const useLogin = () => useContext(LoginProvider);
+export const useLogin = () => useContext(LoginContext);
