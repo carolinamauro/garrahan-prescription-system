3b6fcb30c70e9074f6d03291ec6dc1b4167f1e44
Carolina Mauro <carolinaluciamauro@gmail.com>
2025-11-11 23:43:47 -0300

    agrego login admin y medico con opcion de cerrar la sesion

--- src/app/page.js
+++ src/app/page.js
@@ -11,25 +11,29 @@ export default function Page() {
   const { patients } = usePatients();
   const { setTitle, setSubtitle } = useHeader();
   const router = useRouter();
-  const { loggedIn } = useLogin();
+  const { loggedIn, isAdmin } = useLogin();
 
   useEffect(() => {
-    setTitle('Pacientes');
-    setSubtitle('Menú principal');
-  }, []);
+    if (isAdmin) {
+      setTitle('Administración de Pacientes');
+      setSubtitle('Menú principal - Administrador');
+    } else {
+      setTitle('Pacientes');
+      setSubtitle('Menú principal');
+    }
+  }, [isAdmin]);
 
   const handleEdit = (rowId) => () => {
     router.push(`/pacientes/${rowId}/editar`);
   };
 
   return (
-    <>
-      <DataTable
-        data={patients}
-        tabsList={[]}
-        withActionButtons={loggedIn}
-        columns={getPatientColumns(handleEdit)}
-      />
-    </>
+    isAdmin ? <></>: <DataTable
+      data={patients}
+      tabsList={[]}
+      withActionButtons={loggedIn}
+      columns={getPatientColumns(handleEdit)}
+    />
+
   );
 }
